42968c7633de06b1c51e04261aa5fdf7df987e07
Guillaume Massé <masgui@gmail.com>
2016-05-31 14:36:25 -0400

    yet another hit at #35

--- model/src/main/scala/ch.epfl.scala.index.model/package.scala
+++ model/src/main/scala/ch.epfl.scala.index.model/package.scala
@@ -5,8 +5,21 @@ package object model {
   import upickle.default._
 
   // speed up compilation / avoid File too long errors
-  implicit val rwProject = macroRW[Project]
-  implicit val rwArtifact = macroRW[Artifact]
-  implicit val rwRelease = macroRW[Release]
+
   implicit val rwSemanticVersion = macroRW[SemanticVersion]
+  implicit val rwScalaTargets = macroRW[ScalaTargets]
+  implicit val rwMavenReference = macroRW[MavenReference]
+  implicit val rwISO_8601_Date = macroRW[ISO_8601_Date]
+  implicit val rwReleaseReference = macroRW[Release.Reference]
+  implicit val rwRelease = macroRW[Release]
+  
+
+  implicit val rwArtifactReference = macroRW[Artifact.Reference]
+  implicit val rwDeprecation = macroRW[Deprecation]
+  implicit val rwArtifact = macroRW[Artifact]
+
+  implicit val rwProjectReference = macroRW[Project.Reference]
+  implicit val rwUrl = macroRW[Url]
+  implicit val rwGithubRepo = macroRW[GithubRepo]
+  implicit val rwProject = macroRW[Project]
 }
