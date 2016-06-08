295aba4b682bda7d7ed7563cec93f414b073bde9
Guillaume Massé <masgui@gmail.com>
2016-06-08 17:39:12 -0400

    paginate results a la gooooogle

--- template/src/main/scala/ch.epfl.scala.index.views/package.scala
+++ template/src/main/scala/ch.epfl.scala.index.views/package.scala
@@ -13,4 +13,26 @@ package object html {
     else if(u.startsWith(https)) u.drop(https.length)
     else u
   }
+
+  def pagination(current: Int, total: Int, window: Int): (Option[Int], List[Int], Option[Int]) = {
+    
+    val prev =
+      if(current == 1) None
+      else Some(current -1)
+
+    val next =
+      if(current == total) None
+      else Some(current + 1)
+        
+    val delta = (window - 1) / 2
+    val (start, end) =
+      if(current + delta <= total) {
+        if(current - delta >= 1) (current - delta, current + delta)
+        else (1, window + 1)
+      } else (total - window, total)
+
+    val sels = (start to end).toList
+
+    (prev, sels, next)
+  }
 }
