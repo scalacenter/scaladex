4ab5bcff9c5610e56a554a2e16c4f48c84195fc3
Scyks <scyks@ceow.de>
2016-07-11 22:53:36 +0200

    solved SBT plugin - it works :)
    Thanks @MasseGauillaume :)

--- sbtScaladex/src/main/scala/ScaladexPlugin.scala
+++ sbtScaladex/src/main/scala/ScaladexPlugin.scala
@@ -8,7 +8,7 @@ object ScaladexPlugin extends AutoPlugin {
 
   object autoImport {
 
-    lazy val Scaladex = config("Scaladex") extend Compile
+    lazy val Scaladex = config("scaladex") extend Compile
     lazy val scaladexKeywords = settingKey[Seq[String]]("list of keywords for your package")
     lazy val scaladexDownloadReadme = settingKey[Boolean]("defines if scaladex should download the readme file")
     lazy val scaladexDownloadInfo = settingKey[Boolean]("defines if scaladex should download the project info")
@@ -16,43 +16,41 @@ object ScaladexPlugin extends AutoPlugin {
 
     lazy val baseScaladexSettings = Seq(
       scaladexKeywords := Seq(),
-      scaladexDownloadContributors := false,
-      scaladexDownloadInfo := false,
-      scaladexDownloadReadme := false
+      scaladexDownloadContributors := true,
+      scaladexDownloadInfo := true,
+      scaladexDownloadReadme := true
     ) ++
-      inConfig(Scaladex)(Seq(
-        publishTo := Some("Scaladex" at s"http://localhost:8080/publish?readme=${scaladexDownloadReadme.value}&info=${scaladexDownloadInfo.value}&contributors=${scaladexDownloadContributors.value}${scaladexKeywords.value.map(key => s"&keywords=$key").mkString("")}&path="),
-        publishArtifact in packageBin := false,
-        publishArtifact in packageDoc := false,
-        publishArtifact in packageSrc := false,
-        credentials += Credentials(Path.userHome / ".ivy2" / ".scaladex.credentials")
+      inConfig(Scaladex)(
+        Classpaths.ivyPublishSettings ++
+          Classpaths.ivyBaseSettings ++
+          Seq(
+            publishTo := {
 
-      ))
+              val baseUrl = "http://localhost:8080/publish?"
+              val params = List(
+                "readme" -> scaladexDownloadReadme.value,
+                "info" -> scaladexDownloadInfo.value,
+                "contributors" -> scaladexDownloadContributors.value,
+                "path" -> "" // need to be at the end!
+              )
+              val keywords = scaladexKeywords.value.map(key => "keywords" -> key)
+
+              val url = baseUrl + (keywords ++ params).map {
+                case(k,v) => s"$k=$v"
+              }.mkString("&")
+
+              Some("Scaladex" at url)
+            },
+            publishArtifact in packageBin := true,
+            publishArtifact in packageDoc := true,
+            publishArtifact in packageSrc := true
+
+          )) ++ Seq(
+      packagedArtifacts in Scaladex := (packagedArtifacts in Compile).value.filter(_._2.getName.endsWith(".pom"))
+    )
   }
 
   import autoImport._
-
   override def trigger = allRequirements
-
   override lazy val projectSettings = baseScaladexSettings
-
-
-  publish in Scaladex := {
-
-    println("publish in Scaladex")
-    (publish in Compile).value
-  }
-
-  //	def scaladexResolver: Resolver = {
-
-  //		val readme = scaladexDownloadReadme.value
-  //		val info = scaladexDownloadInfo.value
-  //		val contributors = scaladexDownloadContributors.value
-  //		val keywords = scaladexKeywords.value.map(key => s"&keywords=$key").mkString("")
-  //
-  //		val url = "http://localhost:8080/publish"
-
-  //		"Scaladex" at s"http://localhost:8080/publish?readme=${scaladexDownloadReadme.value}&info=${scaladexDownloadInfo.value}&contributors=${scaladexDownloadContributors.value}${scaladexKeywords.value.map(key => s"&keywords=$key").mkString("")}&path="
-  //	}
-
 }
