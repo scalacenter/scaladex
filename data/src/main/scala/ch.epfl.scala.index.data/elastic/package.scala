05d810f46973b18a859dbdc21c7986c7e68d39dd
Guillaume Massé <masgui@gmail.com>
2016-07-26 15:57:20 -0400

    config factory and readme link

--- data/src/main/scala/ch.epfl.scala.index.data/elastic/package.scala
+++ data/src/main/scala/ch.epfl.scala.index.data/elastic/package.scala
@@ -12,6 +12,8 @@ import org.json4s._
 import org.json4s.native.Serialization
 import org.json4s.native.Serialization.{read, write}
 
+import com.typesafe.config.ConfigFactory
+
 trait ProjectProtocol {
 
   implicit val formats = Serialization.formats(
