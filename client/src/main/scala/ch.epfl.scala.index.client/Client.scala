a1b4095eb2ee7ec880bb3e7acafe33b30093e608
Julien Richard-Foy <julien@richard-foy.fr>
2016-08-04 18:06:41 +0200

    Add support for navigation in the autocompletion list using keys

--- client/src/main/scala/ch.epfl.scala.index.client/Client.scala
+++ client/src/main/scala/ch.epfl.scala.index.client/Client.scala
@@ -4,16 +4,14 @@ package client
 import autowire._
 import api._
 import rpc.AutowireClient
-
 import org.scalajs.dom
-import org.scalajs.dom.{Event, document}
-import org.scalajs.dom.raw.{Element, HTMLInputElement, Node}
+import org.scalajs.dom.{Event, KeyboardEvent, document}
+import org.scalajs.dom.raw.{Element, HTMLInputElement, HTMLUListElement, Node}
 
 import scalatags.JsDom.all._
 import scalajs.concurrent.JSExecutionContext.Implicits.queue
 import scalajs.js.annotation.JSExport
 import scalajs.js.JSApp
-
 import scala.concurrent.Future
 import scala.util.Try
 
@@ -21,6 +19,7 @@ trait ClientBase {
 
   val searchId        = "search"
   val resultElementId = "list-result"
+  var completionSelection: CompletionSelection = CompletionSelection.empty
 
   def getResultList: Option[Element] = getElement(resultElementId)
 
@@ -61,17 +60,22 @@ trait ClientBase {
   def getProjects(query: String): Future[List[Autocompletion]] =
     AutowireClient[Api].search(query).call()
 
-  def showResults(projects: List[Autocompletion]): List[Option[Node]] =
+  def showResults(projects: List[Autocompletion]): List[Option[Node]] = {
+    completionSelection = CompletionSelection(None, projects)
     projects.map {
       case Autocompletion(organization, repository, description) =>
         appendResult(
-            organization,
-            repository,
-            description
+          organization,
+          repository,
+          description
         )
     }
+  }
 
-  def cleanResults(): Unit = getResultList.fold()(_.innerHTML = "")
+  def cleanResults(): Unit = {
+    completionSelection = CompletionSelection.empty
+    getResultList.fold()(_.innerHTML = "")
+  }
 
   @JSExport
   def runSearch(event: dom.Event): Future[List[Option[Node]]] = {
@@ -83,16 +87,64 @@ trait ClientBase {
       .map(showResults)
   }
 
+  def navigate(event: KeyboardEvent): Unit = {
+    if (event.keyCode == 38 && completionSelection.choices.nonEmpty) { // Up
+      moveSelection(
+        completionSelection.selected.map(_ - 1).filter(_ >= 0)
+      )
+    } else if (event.keyCode == 40 && completionSelection.choices.nonEmpty) { // Down
+      moveSelection(
+        completionSelection.selected
+          .fold[Option[Int]](Some(0))(i => Some(math.min(i + 1, completionSelection.choices.size - 1)))
+      )
+    } else if (event.keyCode == 13) { // Enter
+      completionSelection.selected.foreach { selected =>
+        event.preventDefault()
+        val Autocompletion(owner, repo, _) = completionSelection.choices(selected)
+        dom.window.location.assign(s"/$owner/$repo")
+      }
+    } else ()
+
+    def moveSelection(newSelected: Option[Int]): Unit = {
+      event.preventDefault()
+      completionSelection = completionSelection.copy(selected = newSelected)
+      updateSelection()
+    }
+
+    def updateSelection(): Unit = {
+      getResultList.foreach { resultList =>
+        for (i <- 0 until resultList.childElementCount) {
+          val resultElement = resultList.childNodes(i).asInstanceOf[HTMLUListElement]
+          if (completionSelection.selected.contains(i)) {
+            resultElement.classList.add("selected")
+          } else {
+            resultElement.classList.remove("selected")
+          }
+        }
+      }
+    }
+  }
+
   implicit class ElementOps(e: Element) {
     def getInput: HTMLInputElement = get[HTMLInputElement]
     def get[A <: Element]: A       = e.asInstanceOf[A]
   }
+
+  case class CompletionSelection(selected: Option[Int], choices: List[Autocompletion])
+
+  object CompletionSelection {
+    val empty = CompletionSelection(None, Nil)
+  }
+
 }
 
 object Client extends JSApp with ClientBase {
 
   override def main(): Unit = {
-    getSearchBox.foreach(_.addEventListener[Event]("input", runSearch _))
+    getSearchBox.foreach { searchBox =>
+      searchBox.addEventListener[Event]("input", runSearch _)
+      searchBox.addEventListener[KeyboardEvent]("keydown", navigate _)
+    }
   }
 
 }
