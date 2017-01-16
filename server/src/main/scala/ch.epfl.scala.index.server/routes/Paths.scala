5429fd1ec5052ed6466b4d3902c3b25ad0c5a2e2
Adam Voss <vossad01@gmail.com>
2017-01-16 16:51:15 -0600

    Cleanup (mainly inlining) in Paths

--- server/src/main/scala/ch.epfl.scala.index.server/routes/Paths.scala
+++ server/src/main/scala/ch.epfl.scala.index.server/routes/Paths.scala
@@ -1,65 +1,73 @@
 package ch.epfl.scala.index.server.routes
 
-import akka.http.scaladsl.model.headers.HttpCredentials
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Directives.{parameters, path, pathSingleSlash}
 import akka.http.scaladsl.server.PathMatchers.Segment
-import akka.http.scaladsl.server.Directive1
+import akka.http.scaladsl.server.{Directive1, Route}
 import akka.http.scaladsl.unmarshalling.Unmarshaller
-import ch.epfl.scala.index.data.github.GithubCredentials
 import ch.epfl.scala.index.server.UserState
 import ch.megard.akka.http.cors.CorsDirectives.cors
 import org.joda.time.DateTime
 
 class Paths(userState: Directive1[Option[UserState]]) {
 
-  def buildRoutes(behavior: HttpBehavior) = {
-    val userFacingRoutes = concat(
-      frontPagePath(behavior.frontPage),
-      redirectToNoTrailingSlashIfPresent(akka.http.scaladsl.model.StatusCodes.MovedPermanently) {
-        concat(
-          editUpdatePath(behavior.updateProject),
-          editPath(behavior.editProject),
-          legacyArtifactQueryPath(behavior.projectPageArtifactQuery),
-          projectPath(behavior.projectPage),
-          artifactPath(behavior.artifactPage),
-          artifactVersionPath(behavior.artifactPageWithVersion),
-          searchPath(behavior.searchResultsPage),
-          organizationPath(behavior.organizationPage)
-        )
-      }
-    )
+  def apiRoutes(routes: Route*) = concat(routes: _*)
 
+  def publishRoutes(routes: Route*) = concat(routes: _*)
+
+  def badgeRoutes(routes: Route*) = concat(routes: _*)
+
+  def projectRoutes(routes: Route*) = concat(routes: _*)
+
+  def artifactRoutes(routes: Route*) = concat(routes: _*)
+
+  def buildRoutes(behavior: HttpBehavior) = {
     val programmaticRoutes =
       concat(
-        publishStatusPath(behavior.releaseStatus),
-        publishUpdateRoute(behavior.credentialsTransformation)(behavior.publishRelease),
-        apiSearchPath(behavior.projectSearchApi),
-        apiProjectPath(behavior.releaseInfoApi),
-        apiAutocompletePath(behavior.autocomplete),
+        publishRoutes(
+          (get & publish & parameter('path)) (behavior.releaseStatus),
+          (put & publish & publishParameters & entity(as[String]) & extractCredentials.flatMap(behavior.credentialsTransformation)) (behavior.publishRelease)
+        ),
+        apiRoutes(
+          (apiPrefix & cors() & path("search") & get & parameters(('q, 'target, 'scalaVersion, 'scalaJsVersion.?, 'cli.as[Boolean] ? false))) (behavior.projectSearchApi),
+          (apiPrefix & cors() & path("project") & get & parameters(('organization, 'repository, 'artifact.?))) (behavior.releaseInfoApi),
+          (apiPrefix & path("autocomplete") & get & parameter('q)) (behavior.autocomplete)
+        ),
         Assets.routes,
-        versionBadgePath(behavior.versionBadge),
-        queryBadgePath(behavior.countBadge),
+        badgeRoutes(
+          (get & path(Segment / Segment / Segment / "latest.svg") & shieldsParameters) (behavior.versionBadge),
+          (get & path("count.svg") & parameter('q) & shieldsParameters & parameters('subject)) (behavior.countBadge)
+        ),
         behavior.oAuth2routes
+      )
+
+    val userFacingRoutes =
+      concat(
+        (pathSingleSlash & userState) (behavior.frontPage),
+        redirectToNoTrailingSlashIfPresent(akka.http.scaladsl.model.StatusCodes.MovedPermanently) {
+          concat(
+            projectRoutes(
+              (post & path("edit" / Segment / Segment) & userState & pathEnd & formFieldSeq & editFormFields) (behavior.updateProject),
+              (get & path("edit" / Segment / Segment) & userState & pathEnd) (behavior.editProject),
+              (get & path(Segment / Segment) & parameters(('artifact, 'version.?))) (behavior.projectPageArtifactQuery),
+              (get & path(Segment / Segment) & userState & pathEnd) (behavior.projectPage)
+            ),
+            artifactRoutes(
+              (get & path(Segment / Segment / Segment) & userState) (behavior.artifactPage),
+              (get & path(Segment / Segment / Segment / Segment) & userState) (behavior.artifactPageWithVersion)
+            ),
+            (get & path("search") & userState & parameters(('q, 'page.as[Int] ? 1, 'sort.?, 'you.?))) (behavior.searchResultsPage),
+            (get & path(Segment)) (behavior.organizationPage)
+          )
+        }
       )
 
     programmaticRoutes ~ userFacingRoutes
   }
 
-  def frontPagePath = pathSingleSlash & userState
-
-  def searchPath =
-    get & path("search") & userState & parameters(('q, 'page.as[Int] ? 1, 'sort.?, 'you.?))
-
-  val organizationPath = get & path(Segment)
-
   private val shieldsParameters = parameters(('color.?, 'style.?, 'logo.?, 'logoWidth.as[Int].?))
 
-  val queryBadgePath = get & path("count.svg") & parameter('q) & shieldsParameters & parameters('subject)
-
-  def versionBadgePath = get & path(Segment / Segment / Segment / "latest.svg") & shieldsParameters
-
-  def editUpdatePath = post & path("edit" / Segment / Segment) & userState & pathEnd & formFieldSeq & formFields((
+  private val editFormFields = formFields((
     'contributorsWanted.as[Boolean] ? false,
     'keywords.*,
     'defaultArtifact.?,
@@ -69,21 +77,9 @@ class Paths(userState: Directive1[Option[UserState]]) {
     'cliArtifacts.*,
     'customScalaDoc.?))
 
-  def editPath = get & path("edit" / Segment / Segment) & userState & pathEnd
-
-  def projectPath = get & path(Segment / Segment) & userState & pathEnd
-
-  def legacyArtifactQueryPath = get & path(Segment / Segment) & parameters(('artifact, 'version.?))
-
-  def artifactPath = get & path(Segment / Segment / Segment) & userState
-
-  def artifactVersionPath = get & path(Segment / Segment / Segment / Segment) & userState
-
   private val DateTimeUn = Unmarshaller.strict[String, DateTime] { dateRaw =>
     new DateTime(dateRaw.toLong * 1000L)
   }
-
-  private val publish = path("publish")
 
   private val publishParameters = parameters((
     'path,
@@ -95,15 +91,7 @@ class Paths(userState: Directive1[Option[UserState]]) {
     'test.as[Boolean] ? false
   ))
 
-  def publishUpdateRoute(credentialsTransformation: (Option[HttpCredentials]) => Directive1[(GithubCredentials, UserState)]) = {
-    put & publish & publishParameters & entity(as[String]) & extractCredentials.flatMap(credentialsTransformation)
-  }
-
-  val publishStatusPath = get & publish & parameter('path)
+  private val publish = path("publish")
 
   private val apiPrefix = pathPrefix("api")
-
-  val apiAutocompletePath = apiPrefix & path("autocomplete") & get & parameter('q)
-  val apiProjectPath = apiPrefix & cors() & path("project") & get & parameters(('organization, 'repository, 'artifact.?))
-  val apiSearchPath = apiPrefix & cors() & path("search") & get & parameters(('q, 'target, 'scalaVersion, 'scalaJsVersion.?, 'cli.as[Boolean] ? false))
 }
