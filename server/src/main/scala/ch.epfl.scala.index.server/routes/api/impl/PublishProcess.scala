6a2dc958d94141e6091da3cc0b9e7aa2dba44d18
Guillaume Massé <masgui@gmail.com>
2017-07-28 17:59:07 +0200

    Disabled Sonatype auto-indexing until #478 is fixed

--- server/src/main/scala/ch.epfl.scala.index.server/routes/api/impl/PublishProcess.scala
+++ server/src/main/scala/ch.epfl.scala.index.server/routes/api/impl/PublishProcess.scala
@@ -70,12 +70,16 @@ private[api] class PublishProcess(paths: DataPaths,
                       .contains(repo)) {
                   data.writePom(paths)
                   data.deleteTemp()
-                  updateIndex(repo, pom, data).map { _ =>
-                    log.info(s"Published ${pom.organization
-                      .map(_.name)
-                      .getOrElse("")} ${pom.artifactId} ${pom.version}")
-                    (Created, "Published release")
-                  }
+
+                  // updateIndex(repo, pom, data).map { _ =>
+                  //   log.info(s"Published ${pom.organization
+                  //     .map(_.name)
+                  //     .getOrElse("")} ${pom.artifactId} ${pom.version}")
+                  //   (Created, "Published release")
+                  // }
+
+                  // XXX: Disabled indexing #478
+                  Future.successful((Created, "Published release"))
                 } else {
                   log.warn(
                     s"User ${data.userState.user.login} attempted to publish to ${repo.toString}"
