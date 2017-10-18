36ecdc11294b4f259a4109222d3cee951bfde4dc
Guillaume Massé <masgui@gmail.com>
2017-10-18 11:55:25 +0200

    Fetch all versions for all existing artifacts

--- data/src/main/scala/ch.epfl.scala.index.data/central/CentralMissing.scala
+++ data/src/main/scala/ch.epfl.scala.index.data/central/CentralMissing.scala
@@ -2,9 +2,9 @@ package ch.epfl.scala.index
 package data
 
 class CentralMissing(paths: DataPaths) {
-  
+
   // get all artifacts from maven central
   // group by latest version => g, a
   // requestGav(g, a)
-  // 
+  //
 }
