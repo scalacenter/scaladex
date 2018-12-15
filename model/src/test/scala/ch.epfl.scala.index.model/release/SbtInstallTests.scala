55b290966eae39522f8a3fa8264629916e71a4a1
Cosmin Rusu <cr.rusucosmin@gmail.com>
2018-12-15 12:10:18 +0100

    Added Mill resolvers and some tests

--- model/src/test/scala/ch.epfl.scala.index.model/release/SbtInstallTests.scala
+++ model/src/test/scala/ch.epfl.scala.index.model/release/SbtInstallTests.scala
@@ -3,18 +3,7 @@ package release
 
 import org.scalatest._
 
-class SbtInstallTests extends FunSuite {
-  def assert2(a: String, b: String): Unit = {
-    val ok = a == b
-    if (!ok) {
-      println()
-      println("---")
-      println(a + "|")
-      println(b + "|")
-      assert(a == b)
-    }
-  }
-
+class SbtInstallTests extends FunSuite with TestHelpers {
   test("crossFull") {
     val obtained =
       release(
@@ -136,47 +125,4 @@ class SbtInstallTests extends FunSuite {
   }
 
   test("non-standard") {}
-
-  def release(groupId: String,
-              artifactId: String,
-              version: String,
-              artifactName: String,
-              target: Option[ScalaTarget],
-              isNonStandardLib: Boolean = false,
-              resolver: Option[Resolver] = None) = {
-    Release(
-      maven = MavenReference(
-        groupId = groupId,
-        artifactId = artifactId,
-        version = version
-      ),
-      reference = Release.Reference(
-        artifact = artifactName,
-        version = SemanticVersion(version).get,
-        target = target,
-        // Not necessary for the test
-        organization = "GitHub-Org",
-        repository = "GitHub-Repo"
-      ),
-      resolver = resolver,
-      isNonStandardLib = isNonStandardLib,
-      // default/elasticsearch fields
-      name = None,
-      description = None,
-      released = None,
-      licenses = Set(),
-      id = None,
-      liveData = false,
-      scalaDependencies = Seq(),
-      javaDependencies = Seq(),
-      reverseDependencies = Seq(),
-      internalDependencies = Seq(),
-      targetType = "",
-      fullScalaVersion = None,
-      scalaVersion = None,
-      scalaJsVersion = None,
-      scalaNativeVersion = None,
-      sbtVersion = None
-    )
-  }
 }
