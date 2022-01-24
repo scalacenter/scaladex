e2d984861e825204738addecd81e489c836bfc49
Adrien Piquerez <adrien.piquerez@gmail.com>
2022-01-24 16:47:00 +0100

    Remove Bintray code
    We will not need that code anymore since Bintray sunset:
    https://jfrog.com/blog/into-the-sunset-bintray-jcenter-gocenter-and-chartcenter/
    
    Also remove credentials in DataPaths and FilesystemConfig

--- modules/infra/src/main/scala/scaladex/infra/config/FilesystemConfig.scala
+++ modules/infra/src/main/scala/scaladex/infra/config/FilesystemConfig.scala
@@ -5,14 +5,13 @@ import java.nio.file.Paths
 
 import com.typesafe.config.Config
 
-case class FilesystemConfig(temp: Path, index: Path, contrib: Path, credentials: Path)
+case class FilesystemConfig(temp: Path, index: Path, contrib: Path)
 
 object FilesystemConfig {
   def from(config: Config): FilesystemConfig = {
     val temp = Paths.get(config.getString("scaladex.filesystem.temp"))
     val index = Paths.get(config.getString("scaladex.filesystem.index"))
     val contrib = Paths.get(config.getString("scaladex.filesystem.contrib"))
-    val credentials = Paths.get(config.getString("scaladex.filesystem.credentials"))
-    FilesystemConfig(temp, index, contrib, credentials)
+    FilesystemConfig(temp, index, contrib)
   }
 }
