3599ba0803c19187f9b4d25f964c8b3a315044d4
James Yoo <jyoo980@gmail.com>
2022-03-13 15:36:13 -0700

    Fixing forward declaration in migration script

--- modules/infra/src/main/resources/migrations/V7__add_user_sessions.sql
+++ modules/infra/src/main/resources/migrations/V7__add_user_sessions.sql
@@ -1,19 +1,18 @@
 CREATE TYPE ENV AS ENUM ('local', 'dev', 'prod');
 
-CREATE TABLE user_sessions(
-    user_id VARCHAR NOT NULL,
-    repos VARCHAR,
-    orgs VARCHAR,
-    login VARCHAR(39) NOT NULL,
-    env ENV NOT NULL,
-    FOREIGN KEY (login) REFERENCES user_info(login),
-    PRIMARY KEY (user_id)
-);
-
 CREATE TABLE user_info(
     login         VARCHAR(39) NOT NULL,
     name          VARCHAR(100),
     avatar_url    VARCHAR NOT NULL,
     secret        VARCHAR NOT NULL,
     PRIMARY KEY (login)
+);
+CREATE TABLE user_sessions(
+    user_id  VARCHAR NOT NULL,
+    repos    VARCHAR,
+    orgs     VARCHAR,
+    login    VARCHAR(39) NOT NULL,
+    env ENV  NOT NULL,
+    FOREIGN KEY (login) REFERENCES user_info(login),
+    PRIMARY KEY (user_id)
 );
