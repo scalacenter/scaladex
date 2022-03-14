d780b2fd5a3fac249e4fd56fc2a1b82d2d13053f
James Yoo <jyoo980@gmail.com>
2022-03-14 09:49:06 -0700

    Removing separate user_info table, adding to user_sessions

--- modules/infra/src/main/resources/migrations/V7__add_user_sessions.sql
+++ modules/infra/src/main/resources/migrations/V7__add_user_sessions.sql
@@ -1,18 +1,10 @@
-CREATE TYPE ENV AS ENUM ('local', 'dev', 'prod');
-
-CREATE TABLE user_info(
+CREATE TABLE user_sessions(
+    user_id       VARCHAR NOT NULL,
+    repos         VARCHAR,
+    orgs          VARCHAR,
     login         VARCHAR(39) NOT NULL,
     name          VARCHAR(100),
     avatar_url    VARCHAR NOT NULL,
     secret        VARCHAR NOT NULL,
-    PRIMARY KEY (login)
-);
-CREATE TABLE user_sessions(
-    user_id  VARCHAR NOT NULL,
-    repos    VARCHAR,
-    orgs     VARCHAR,
-    login    VARCHAR(39) NOT NULL,
-    env ENV  NOT NULL,
-    FOREIGN KEY (login) REFERENCES user_info(login),
     PRIMARY KEY (user_id)
 );
