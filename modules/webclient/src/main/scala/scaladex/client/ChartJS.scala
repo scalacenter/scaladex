4ec112c957b564d92bab5a7051880f841f6490cc
David Kunzmann <davidkunzmann@Davids-MacBook-Pro-2.local>
2022-08-02 07:03:42 +0200

    chore: added chartjs date adapter

--- modules/webclient/src/main/scala/scaladex/client/ChartJS.scala
+++ modules/webclient/src/main/scala/scaladex/client/ChartJS.scala
@@ -2,21 +2,19 @@ package scaladex.client
 
 import scala.scalajs.js
 import scala.scalajs.js.JSConverters._
-import org.scalajs.dom.raw.CanvasGradient
+
 import org.scalajs.dom
+import org.scalajs.dom.raw.CanvasGradient
 
 @js.native
 trait ChartDataset extends js.Object {
   def label: String = js.native
-  def color: CanvasGradient = js.native
-  def data: js.Array[Double] = js.native
-  def datasets: js.Array[ChartDataset] = js.native
-  def pointRadius: Int = js.native
+  def data: js.Array[DataPoint] = js.native
 }
 
 object ChartDataset {
   def apply(
-      data: Seq[Double],
+      data: Seq[DataPoint],
       label: String
   ): ChartDataset =
     js.Dynamic
@@ -26,18 +24,34 @@ object ChartDataset {
       )
       .asInstanceOf[ChartDataset]
 }
+@js.native
+trait DataPoint extends js.Object {
+  def x: Double = js.native
+  def y: Double = js.native
+}
+
+object DataPoint {
+  def apply(
+      x: Double,
+      y: Double
+  ): DataPoint =
+    js.Dynamic
+      .literal(
+        x = x,
+        y = y
+      )
+      .asInstanceOf[DataPoint]
+}
 
 @js.native
 trait ChartData extends js.Object {
-  def labels: js.Array[String] = js.native
   def datasets: js.Array[ChartDataset] = js.native
 }
 
 object ChartData {
-  def apply(labels: Seq[String], datasets: Seq[ChartDataset]): ChartData =
+  def apply(datasets: Seq[ChartDataset]): ChartData =
     js.Dynamic
       .literal(
-        labels = labels.toJSArray,
         datasets = datasets.toJSArray
       )
       .asInstanceOf[ChartData]
@@ -65,14 +79,16 @@ object ChartConfig {
 trait ChartOptions extends js.Object {
   def plugins: PluginOptions = js.native
   def elements: ElementOptions = js.native
+  def scales: ScaleOptions = js.native
 }
 
 object ChartOptions {
-  def apply(plugins: PluginOptions, elements: ElementOptions): ChartOptions =
+  def apply(plugins: PluginOptions, elements: ElementOptions, scales: ScaleOptions): ChartOptions =
     js.Dynamic
       .literal(
         plugins = plugins,
-        elements = elements
+        elements = elements,
+        scales = scales
       )
       .asInstanceOf[ChartOptions]
 }
@@ -83,11 +99,7 @@ trait PluginOptions extends js.Object {
 }
 object PluginOptions {
   def apply(legend: LegendOptions): PluginOptions =
-    js.Dynamic
-      .literal(
-        legend = legend
-      )
-      .asInstanceOf[PluginOptions]
+    js.Dynamic.literal(legend = legend).asInstanceOf[PluginOptions]
 }
 
 @js.native
@@ -95,14 +107,10 @@ trait ElementOptions extends js.Object {
   def line: LineOptions = js.native
   def point: PointOptions = js.native
 }
+
 object ElementOptions {
   def apply(line: LineOptions, point: PointOptions): ElementOptions =
-    js.Dynamic
-      .literal(
-        line = line,
-        point = point
-      )
-      .asInstanceOf[ElementOptions]
+    js.Dynamic.literal(line = line, point = point).asInstanceOf[ElementOptions]
 }
 
 @js.native
@@ -112,24 +120,66 @@ trait LineOptions extends js.Object {
 object LineOptions {
   def apply(borderColor: CanvasGradient): LineOptions =
     js.Dynamic
-      .literal(
-        borderColor = borderColor
-      )
+      .literal(borderColor = borderColor)
       .asInstanceOf[LineOptions]
 }
 
 @js.native
 trait PointOptions extends js.Object {
   def radius: Int = js.native
+  def borderColor: String = js.native
 }
 object PointOptions {
-  def apply(radius: Int): PointOptions =
+  def apply(radius: Int, borderColor: String): PointOptions =
     js.Dynamic
       .literal(
-        radius = radius
+        radius = radius,
+        borderColor = borderColor
       )
       .asInstanceOf[PointOptions]
 }
+
+@js.native
+trait ScaleOptions extends js.Object {
+  def x: XAxisOptions = js.native
+}
+object ScaleOptions {
+  def apply(x: XAxisOptions): ScaleOptions =
+    js.Dynamic
+      .literal(
+        x = x
+      )
+      .asInstanceOf[ScaleOptions]
+}
+
+@js.native
+trait XAxisOptions extends js.Object {
+  def `type`: String = js.native
+  def time: TimeOptions = js.native
+}
+object XAxisOptions {
+  def apply(`type`: String, time: TimeOptions): XAxisOptions =
+    js.Dynamic
+      .literal(
+        `type` = `type`,
+        time = time
+      )
+      .asInstanceOf[XAxisOptions]
+}
+
+@js.native
+trait TimeOptions extends js.Object {
+  def unit: String = js.native
+}
+object TimeOptions {
+  def apply(unit: String): TimeOptions =
+    js.Dynamic
+      .literal(
+        unit = unit
+      )
+      .asInstanceOf[TimeOptions]
+}
+
 @js.native
 trait LegendOptions extends js.Object {
   def display: Boolean = js.native
--- modules/webclient/src/main/scala/scaladex/client/Sparkline.scala
+++ modules/webclient/src/main/scala/scaladex/client/Sparkline.scala
@@ -1,13 +1,12 @@
 package scaladex.client
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+
 import org.scalajs.dom
 import org.scalajs.dom.document
 
-import java.time.LocalDateTime
-import java.time.ZoneOffset
-import java.time.LocalDate
-
 /**
-  * Find all commit activity canvas tag and draw the commit activity sparkline
+ * Create the ChartJS line chart with the commit activity
   */
 object Sparkline {
 
@@ -15,15 +14,16 @@ object Sparkline {
     val canvas = document.querySelector("#commit-activity").asInstanceOf[dom.raw.HTMLCanvasElement]
     val commits = canvas.getAttribute("data-commit-activity-count").split(",").map(_.toDouble).toSeq
     val startingDay = canvas.getAttribute("data-commit-activity-starting-day")
-    val endDay = canvas.getAttribute("data-commit-activity-last-day")
-    if (startingDay.nonEmpty && endDay.nonEmpty) {
-      val firstDay = LocalDateTime.ofEpochSecond(startingDay.toLong, 0, ZoneOffset.UTC).toLocalDate()
-      val lastDay = LocalDateTime.ofEpochSecond(endDay.toLong, 0, ZoneOffset.UTC).toLocalDate()
-      val days = firstDay.toEpochDay.to(lastDay.toEpochDay).map(LocalDate.ofEpochDay(_).getMonth())
+    if (startingDay.nonEmpty) {
+      val startDate = Instant.ofEpochSecond(startingDay.toLong)
+      val data = commits.zipWithIndex.map {
+        case (commit, index) => DataPoint(startDate.plus(index, ChronoUnit.DAYS).toEpochMilli().toDouble, commit)
+      }
       val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
       val grad = ctx.createLinearGradient(0, 0, canvas.width, canvas.height)
       grad.addColorStop(0, "rgb(0, 122, 201)"); // Initial path colour
       grad.addColorStop(1, "rgb(0, 201, 114)"); // End stroke colour
+
       val chartOptions = ChartOptions(
         PluginOptions(LegendOptions(display = false, FontOptions(size = 10))),
         elements = ElementOptions(
@@ -31,7 +31,16 @@ object Sparkline {
             borderColor = grad
           ),
           point = PointOptions(
-            radius = 0
+            radius = 2,
+            borderColor = "rgb(0,201,114)"
+          )
+        ),
+        scales = ScaleOptions(
+          x = XAxisOptions(
+            `type` = "time",
+            time = TimeOptions(
+              unit = "month"
+            )
           )
         )
       )
@@ -39,7 +48,7 @@ object Sparkline {
         new Chart(
           ctx,
           Chart.Line(
-            ChartData(days.map(_.toString), Seq(ChartDataset(commits, "Commit count"))),
+            ChartData(Seq(ChartDataset(data, "Commit count"))),
             chartOptions
           )
         )
