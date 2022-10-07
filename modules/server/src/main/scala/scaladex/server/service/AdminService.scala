21e3e230b87bfc4facb7a99bfab4442c0a75b2ab
Marco F <mfirry@users.noreply.github.com>
2022-10-07 17:36:40 +0200

    resolves #1032

--- modules/server/src/main/scala/scaladex/server/service/AdminService.scala
+++ modules/server/src/main/scala/scaladex/server/service/AdminService.scala
@@ -1,16 +1,13 @@
 package scaladex.server.service
 import scala.concurrent.Future
-
 import akka.actor.ActorSystem
 import com.typesafe.scalalogging.LazyLogging
-import scaladex.core.model.Artifact
-import scaladex.core.model.Env
-import scaladex.core.model.UserState
+import scaladex.core.model.Project.{Organization, Repository, Settings}
+import scaladex.core.model.{Artifact, Env, GithubInfo, GithubResponse, GithubStatus, Project, UserState}
 import scaladex.core.service.GithubClient
 import scaladex.core.service.SchedulerDatabase
 import scaladex.core.service.SearchEngine
 import scaladex.core.util.ScalaExtensions._
-import scaladex.server.service.JobScheduler
 import scaladex.view.Job
 import scaladex.view.Task
 
@@ -73,6 +70,58 @@ class AdminService(
   }
 
   def allTaskStatuses: Seq[Task.Status] = tasks.map(_.status)
+
+  def addProjectNoArtifact(reference: Project.Reference, user: UserState
+                          ): Unit = {
+
+    /**
+     * Build empty Settings
+     * @return
+     */
+    def buildSettings(): Settings = Settings(
+      false, None, false, None, List.empty, false,false, Set.empty, Set.empty, None, None)
+
+    /**
+     * Give a GithubInfo create a scaladex Project
+     * @param info
+     * @return
+     */
+    def buildProject(info: GithubInfo): Project = new Project(
+      organization = Organization(""), // FIXME
+      repository = Repository(""), // FIXME
+      creationDate = None, // FIXME
+      GithubStatus.Ok(java.time.Instant.now),
+      githubInfo = Some(info),
+      settings = buildSettings()
+    )
+
+    val input = Seq("Organization" -> reference.organization.value, "Repository" -> reference.repository.value)
+
+    val task = TaskRunner.run(Task.missingProjectNoArtifact, user.info.login, input) { () =>
+      githubClientOpt match {
+        case None =>
+          throw new Exception("Failed because 1 ??")
+        case Some(githubClient) =>
+          githubClient.getProjectInfo(reference).flatMap {
+            case GithubResponse.Ok((_, info)) =>
+              info.scalaPercentage match {
+                case None =>
+                  throw new Exception("Failed because 2 ???")
+                case Some(percentage) =>
+                  if(percentage <= 0) {
+                    throw new Exception("Failed because 3 ???")
+                  } else {
+                    val project = buildProject(info)
+                    database.insertProject(project)
+                      .flatMap(_ => searchSynchronizer.syncProject(reference))
+                      .map(_ => "success")
+                  }
+              }
+          }
+      }
+    }
+    tasks = tasks :+ task
+  }
 
   private def updateProjectCreationDate(): Future[String] =
     for {
