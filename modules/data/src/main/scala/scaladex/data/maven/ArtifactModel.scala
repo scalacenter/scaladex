6a7f0def3a79d22f1b9161bd769f0458be0f3779
Siddharth Ingle <29544714+skingle@users.noreply.github.com>
2024-06-02 01:52:22 +0100

    refactor: moved Contributor model to shared models

--- modules/data/src/main/scala/scaladex/data/maven/ArtifactModel.scala
+++ modules/data/src/main/scala/scaladex/data/maven/ArtifactModel.scala
@@ -1,7 +1,7 @@
 package scaladex.data
 package maven
 
-import scaladex.core.model.{Artifact, Url}
+import scaladex.core.model.{Artifact, Contributor, Url}
 
 /** Abstract model of a released artifact. Initially modeled after the POM model. Tweaked to fit with ivy.xml descriptors */
 // POM Model
@@ -69,29 +69,6 @@ case class License(
     comments: Option[String] = None
 )
 
-/**
- * Description of a person who has contributed to the project, but who does not
- *    have commit privileges. Usually, these contributions come in the form of patches submitted.
- */
-case class Contributor(
-    name: Option[String],
-    email: Option[String],
-    url: Option[String],
-    organization: Option[String],
-    organizationUrl: Option[String],
-    roles: List[String],
-    /*
-    The timezone the contributor is in. Typically, this is a number in the range
-    <a href="http://en.wikipedia.org/wiki/UTC%E2%88%9212:00">-12</a> to <a href="http://en.wikipedia.org/wiki/UTC%2B14:00">+14</a>
-    or a valid time zone id like "America/Montreal" (UTC-05:00) or "Europe/Paris" (UTC+01:00).
-     */
-    timezone: Option[String],
-    // Properties about the contributor, such as an instant messenger handle.
-    properties: Map[String, String],
-    // Developer
-    // The unique ID of the developer in the SCM.
-    id: Option[String]
-)
 
 case class Dependency(
     groupId: String, // org.apache.maven
