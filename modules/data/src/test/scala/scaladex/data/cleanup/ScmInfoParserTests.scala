b267622aa30e161bf4f156057ef4d42f266d1543
Arnav Sharma <2006arnavsharma@gmail.com>
2025-12-02 23:46:50 +0530

    Clean up comments in ScmInfoParserTests
    Removed comments for different SCM protocols in tests.

--- modules/data/src/test/scala/scaladex/data/cleanup/ScmInfoParserTests.scala
+++ modules/data/src/test/scala/scaladex/data/cleanup/ScmInfoParserTests.scala
@@ -7,39 +7,33 @@ import org.scalatest.matchers.should.Matchers
 class ScmInfoParserTests extends AnyFunSpec with Matchers:
   describe("ScmInfoParse") {
     it("correctly parse valid SCM strings") {
-      // Implicit protocol
       ScmInfoParser
         .parseRawConnection("scm:git:git@github.com:foobarbuz/example.git")
         .map(_.toString) shouldBe Some("foobarbuz/example")
-      // HTTPS
       ScmInfoParser
         .parseRawConnection("scm:https://github.com/foobarbuz/example.git")
         .map(_.toString) shouldBe Some("foobarbuz/example")
       ScmInfoParser
         .parseRawConnection("scm:https://github.com/foobarbuz/example")
         .map(_.toString) shouldBe Some("foobarbuz/example")
-      // Git
       ScmInfoParser
         .parseRawConnection("scm:git:git://github.com:foobarbuz/example.git")
         .map(_.toString) shouldBe Some("foobarbuz/example")
       ScmInfoParser
         .parseRawConnection("scm:git://github.com:foobarbuz/example.git")
         .map(_.toString) shouldBe Some("foobarbuz/example")
-      // SSH
       ScmInfoParser
         .parseRawConnection("scm:git:ssh://git@github.com:foobarbuz/example.git")
         .map(_.toString) shouldBe Some("foobarbuz/example")
       ScmInfoParser
         .parseRawConnection("scm:git:ssh://github.com:foobarbuz/example.git")
         .map(_.toString) shouldBe Some("foobarbuz/example")
-      // Unknown protocol
       ScmInfoParser
         .parseRawConnection("scm:git:unknown://git@github.com:foobarbuz/example.git")
         .map(_.toString) shouldBe None
       ScmInfoParser
         .parseRawConnection("scm:git:unknown://github.com:foobarbuz/example.git")
         .map(_.toString) shouldBe None
-      
       ScmInfoParser
         .parseRawConnection("scm:git@github.com:mghmay/play-json-shaper.git")
         .map(_.toString) shouldBe Some("mghmay/play-json-shaper")
